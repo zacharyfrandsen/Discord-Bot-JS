5c8c131306ebe897ebd6f8537f697029b76b1a25
zacharyfrandsen <70166807+zacharyfrandsen@users.noreply.github.com>
2022-07-13 14:49:36 -0700

    Delete Counters directory

--- Counters/memberCounter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) => {
-    const guild = client.guilds.cache.get('829799190510829569');
-    setInterval(() => {
-        const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('865335541225226280');
-        channel.setName(`Members : ${memberCount.toLocaleString()}`);
-        console.log('Updating Member Count');
-    }, 3600000);
-}
