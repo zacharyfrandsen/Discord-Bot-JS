72029dc9fb924bc86ed5d5c3e2eafdd793bbb924
zacharyfrandsen <70166807+zacharyfrandsen@users.noreply.github.com>
2022-07-14 13:09:18 -0700

    Add files via upload

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-module.exports = (message, client, Discord) => {
+module.exports = (client, Discord) => {
     const load_dir = (dirs) => {
         const eventFiles = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
         for(const file of eventFiles){
